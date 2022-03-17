5755a1be4f151870cf4a75499bd886c1da1d6ebe
Jud Stewart <juds@vt.edu>
2022-03-17 16:06:58 -0400

    Began origins page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OriginsComponent } from './origins/origins.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    OriginsComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/origins/origins.component.spec.ts
+++ src/app/origins/origins.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { OriginsComponent } from './origins.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('OriginsComponent', () => {
+  let component: OriginsComponent;
+  let fixture: ComponentFixture<OriginsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ OriginsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(OriginsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
