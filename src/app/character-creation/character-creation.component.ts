02aded5fa8b307fcc598a0ac693889368eb5e790
JudStewart <tjudsonstewart@gmail.com>
2022-06-23 12:01:05 -0400

    Added gender selection and reflected it in the name randomization

--- src/app/character-creation/character-creation.component.ts
+++ src/app/character-creation/character-creation.component.ts
@@ -1,31 +1,52 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import * as Gen from 'aurinn-train-content-generator';
-import {races, classes} from 'aurinn-train-content-generator/dist/interfaces/interfaces'
+import {
+  races,
+  classes,
+  IGender,
+} from 'aurinn-train-content-generator/dist/interfaces/interfaces';
 
 @Component({
   selector: 'app-character-creation',
   templateUrl: './character-creation.component.html',
-  styleUrls: ['./character-creation.component.scss']
+  styleUrls: ['./character-creation.component.scss'],
 })
 export class CharacterCreationComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
 
-  constructor(private route: ActivatedRoute) { }
-  
-  die = "\uD83C\uDFB2";
+  die = '\uD83C\uDFB2';
   origin: any;
-  name = ''
-  race = ''
-  
-  gen = Gen
-  races = races
-    
+  name = '';
+  race = '';
+  genderCustom = '';
+  genderChoice = '';
+
+  gen = Gen;
+  races = races;
+  genders = ['Female', 'Male', 'Other'];
+
   ngOnInit(): void {
-    this.origin = JSON.parse(this.route.snapshot.paramMap.get('origin')!)
+    this.origin = JSON.parse(this.route.snapshot.paramMap.get('origin')!);
   }
 
-  randomName()
-  {
-    name = Gen.Names.generate()['name']
+  randomName() {
+    let genderVal = undefined;
+
+    switch (this.genderChoice) {
+      case 'Male':
+      case 'Female':
+        genderVal = this.genderChoice.toLocaleLowerCase() as IGender;
+        break;
+      case 'Other':
+      case '':
+      default:
+        break;
+    }
+
+    this.name = Gen.Names.generate({
+      race: this.race != '' ? this.race : undefined,
+      gender: genderVal,
+    })['name'];
   }
 }
